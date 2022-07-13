00430527a3491688cad73a7d0b85536cab25a177
Anderson-Abreu <prof.andersonmabreu@gmail.com>
2022-07-13 10:55:14 -0300

    Heranca

---
+++ Heranca/TesteHeranca.java
@@ -0,0 +1,10 @@
+package Heranca;
+
+public class TesteHeranca {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- POO/ExemploPOO.java
+++ POO/ExemploPOO.java
@@ -6,6 +6,7 @@ public class ExemploPOO {
 		Caneta bic = new Caneta("Vermelho",0.7);
 		Caneta kilometrica = new Caneta("Azul",1.0);
 		Caneta c1 = new Caneta("Dourada", 0.5);
+		Caneta c2 = new Caneta("preta", 1.0);
 		
 		System.out.println("Caneta bic");
 		bic.setCarga(35);
@@ -17,7 +18,13 @@ public class ExemploPOO {
 		kilometrica.estado();
 
 		System.out.println("\nCaneta C1");
+		c1.tampar();
+		c1.setCarga(20);
 		c1.estado();
+	
+		System.out.println("\nCaneta C2");
+		c2.setCarga(100);
+		System.out.println(c2.getCarga());
 		
 	}
 
